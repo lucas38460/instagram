4a7b23ef17c70aebd5f2f97fbb02271be9c176f1
Lux <92374048+lucas38460@users.noreply.github.com>
2022-06-14 14:55:19 +0200

    mise en lien + depart objet

---
+++ main.js
@@ -0,0 +1,29 @@
+const post = [
+  {
+    isBooked: false,
+    pseudo: "Melanight",
+    isSponsored: true,
+    avatar: "http://via.placeholder.com/50",
+    imgUrl: "http://via.placeholder.com/350",
+    isLiked: false,
+    likes: 380,
+  },
+  {
+    isBooked: true,
+    pseudo: "LucieLpb",
+    isSponsored: false,
+    avatar: "http://via.placeholder.com/50",
+    imgUrl: "http://via.placeholder.com/350",
+    isLiked: false,
+    likes: 667,
+  },
+  {
+    isBooked: false,
+    pseudo: "k_rlos",
+    isSponsored: false,
+    avatar: "http://via.placeholder.com/50",
+    imgUrl: "http://via.placeholder.com/350",
+    isLiked: false,
+    likes: 666,
+  },
+];
